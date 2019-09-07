5f5d9274374c64978c35b4a8c2fe3a0153606e04
Stuart Payne <paynestu@gmail.com>
2019-09-07 13:03:19 +0100

    main.js changes

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import router from 'vue-router';
 
 Vue.config.productionTip = false
+
+Vue.use(router);
 
 new Vue({
   vuetify,
