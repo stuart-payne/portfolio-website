cb8f75d66f11bc48c1b054cf3f58de9137f2fdda
Stuart Payne <paynestu@gmail.com>
2019-10-09 18:41:30 +0100

    content added

--- src/components/Website.vue
+++ src/components/Website.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="projView">
     <h2 class="mb-4 accent--text">Portfolio Website</h2>
+    <p><b>Github here: </b><a>link will be here</a></p>
     <p>
       I needed a website to serve as a quick and concise access point for the projects which I have undertaken; 
       allowing for people interested in my work to find it easily. The requirements for it were for it to be simple,
@@ -31,6 +32,33 @@
       <img :src="require('../assets/codeSnippets/routing.png')"/>
       <p class="imgCap">The code used for the configuration of the Vue router</p>
     </div>
+    <p>
+      The components used in the routing are all vue files which are lazily loaded into the the main.js file.
+      Using lazy loading we can ensure that the file size stays low and that these components are only loaded if actually selected for viewing.
+    </p>
+    <div class="imgDisplay">
+      <img :src="require('../assets/codeSnippets/components.png')"/>
+      <p class="imgCap">Lazy loading of components</p>
+    </div>
+    <p> 
+      The project list on the home page is list rendered using a js file containing an array of project objects.
+      These objects contain the information needed for the project card components on allow me to easily add to the array if I'm adding a new project without having to touch any Vue or html code. 
+    </p>
+    <div class="imgDisplay">
+      <img :src="require('../assets/codeSnippets/projects.png')"/>
+      <p class="imgCap">Project array used for list rendering</p>
+    </div>
+    <div class="imgDisplay">
+      <img :src="require('../assets/codeSnippets/projectCard.png')"/>
+      <p class="imgCap">List rendering for project cards</p>
+    </div>
+    <p>
+      The list rendering only will show four cards at a time so the array in renders from is a comnputed property which returns an array which returns 4 project
+      objects depending on the starting index and end index. These indices are changed by the up and down button by the project list, allowing the user to navigate through the projects.
+    </p>
+    <p>
+      With this setup, it is trivial for my to add nerw projects, just add the component and routing to main.js, create a vue file with the content and connect it with an object in the projects files.
+    </p>
   </div>
 </template>
 
--- src/components/Zelda.vue
+++ src/components/Zelda.vue
@@ -1,6 +1,42 @@
 <template>
-  <div>
-      Zelda
+  <div class="projView">
+      <h2 class="mb-4 accent--text">Zelda Clone!</h2>
+      <p> 
+        The goal of this project is to crate a very simple and short demo for a Zelda-like game.
+        What this mean in this context? A 2D, top-down, sprite-based, pixel art game where the game world
+        consists of tilemaps. The gameplay has basic attacking with a sword and a few puzzles. Its not going 
+        to win any game of the year awards but its a fun experience to make. 
+      </p>
+      <p> 
+        Unity was the engine I chose to use. Unity is prodominantly a 3D engine but it does have support for 2D stuff. While this support is limited
+        and not as fully fleshed out as some 2D engines made for the purpose, Unity is used so widely in the games industry, using this 
+        project to gain familiarity with it seemed of higher value than using a more niche engine that would do the job better. That being said, Unity has been adding
+        more 2D features so it might catch up anyway to other engines.
+      </p>
+      <p>
+        First thing I tackled was the character animations. Starting with spritesheets, I needed to create the animations 
+        and the triggers for those animations. I chose two approaches, first for the player, I used the Unity animator which works as essentially a 
+        state machine with a visual editor.
+      </p>
+      <div class="imgDisplay">
+        <img :src="require('../assets/codeSnippets/zelda/animator.png')"/>
+        <p class="imgCap">Lazy loading of components</p>
+      </div>
+      <p>
+        Above shows the web for the player character. Each node is for a seperate animation that also has four directions for each animation (up, down, right, left)
+        that corresponds to seperate sprites. The parameters dirY and dirX determine which directional sprite is used with each node having a blend tree that uses these values.
+      </p>
+      <div class="imgDisplay">
+        <img :src="require('../assets/codeSnippets/zelda/blendTree.png')"/>
+        <p class="imgCap">Blend tree for player animation</p>
+      </div>
+      <p> We can then use the player input to determine these parameters. The player controller will set the arrow keys to both move the player in terms
+        of velocity and also set the there parameters in the animator to get the correct sprites and animations for those movements.
+      </p>
+      <div class="imgDisplay">
+        <img :src="require('../assets/codeSnippets/zelda/playerController.png')"/>
+        <p class="imgCap">C# code for player input</p>
+      </div>
   </div>
 </template>
 
