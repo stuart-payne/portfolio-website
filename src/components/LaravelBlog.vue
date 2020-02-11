e3a8511fd543e0606c8bdbef85a9e979180e87b8
Stuart Payne <paynestu@gmail.com>
2020-02-11 17:01:00 +0000

    added new articles

---
+++ src/components/LaravelBlog.vue
@@ -0,0 +1,18 @@
+<template>
+      <div class="projView">
+        <h2 class="accent--text">Blog Site made with Laravel</h2>
+        <p>
+        Full code here: <a href="https://github.com/stuart-payne/laravelblog">github</a>
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
