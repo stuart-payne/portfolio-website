6ff021113be311df4e6dcfb43621ba53c3c4808a
Stuart Payne <paynestu@gmail.com>
2020-02-12 13:33:35 +0000

    Completed new articles

--- src/components/GraphQLjs.vue
+++ src/components/GraphQLjs.vue
@@ -5,8 +5,44 @@
         Full code here: <a href="https://github.com/stuart-payne/simple-graphql-js">github</a>
         </p>
         <!-- intro -->
+        <p>
+            Objective of this exercise was to introduce myself to GraphQL to be used as an alternative to traditional methods of building a web API. I decided to use Express as the web server for this exercise
+            due to the ease of creating something simple as well as the large amount of support from graphql itself.
+        </p>
+        <p>
+            There are three parts that a GraphQL API requires to function. First is the schema that defines the overall structure of the data as well as the fields and what type they are as well as the possible Queries
+            that can be make and what arguments can be entered. Below you can see the type Person which is the primary object that is stored in the database fro the server. The Query type then exposes all the possible
+            ways that a client can request data, here we have the ability to request data by each field that person holds as an argument. The last part is the resolver which is reponsible for how the server actually
+            handles the request and database access.
+        </p>
+        <div class="imgDisplay">
+            <img :src="require('../assets/codeSnippets/GraphQLjs/Schema.png')"/>
+            <p class="imgCap">Schema code!</p>
+        </div>
         <!-- Query API -->
+        <p>
+            Below is the resolver functions. These match the Query type which is also shown above. Each function dictates how exactly the server gets the information. Being a simple example, we are only accessing
+            an array which holds the Person data. In a proper website, this is where the database access would happen.
+        </p>
+        <div class="imgDisplay">
+            <img :src="require('../assets/codeSnippets/GraphQLjs/Resolver.png')"/>
+            <p class="imgCap">Functions used to resolve the queries</p>
+        </div>
         <!-- Resolvers -->
+        <p>
+            Using graphiql, we get an easy GraphQL client which we can use to test our server. Below is a query to see what data is returned.
+        </p>
+        <div class="imgDisplay">
+            <img :src="require('../assets/codeSnippets/GraphQLjs/Query.png')"/>
+            <p class="imgCap">Query in Graphiql</p>
+        </div>
+        <div class="imgDisplay">
+            <img :src="require('../assets/codeSnippets/GraphQLjs/Data.png')"/>
+            <p class="imgCap">Some of the data returned by our successful query!</p>
+        </div>
+        <p>
+            And there we have a working GraphQL API albeit a very barebones one.
+        </p>
         <!-- Example Queries -->
     </div>
 </template>
--- src/components/LaravelBlog.vue
+++ src/components/LaravelBlog.vue
@@ -21,15 +21,59 @@
             First step was creating the migration for Articles. A migration in Laravel sets up the table that the article data will be stored in including the fields and their data types. This allows you 
             to set up a database without writing any raw SQL. In the picture below, you can see the code used for the article table in this website.
         </p>
+        <div class="imgDisplay">
+            <img :src="require('../assets/codeSnippets/laravelBlog/Migration.png')"/>
+            <p class="imgCap">Migration code used to build the database</p>
+        </div>
         <!-- Migration code -->
         <p>
-            Next, we need to Eloquent model that will be a primary tool for accessing the table. Here the code is very simple but the important part is the HHHHHHHHHHHHHHHHHHHHHHHHHH which allows us to use 
-            mass assignment. This will allow us easier database operations in our controller.
+            Next, we need to Eloquent model that will be a primary tool for accessing the table. Here the code is very simple but the important part is the empty guarded property which allows us to use 
+            mass assignment. This will allow us easier database operations in our controller. We also define the relationships to the other models we have, these being Users, which an article can only belong to v-once
+            and Comments which an article owns and can have many. Eloquent provides nice methods to describe these.
         </p>
+        <div class="imgDisplay">
+            <img :src="require('../assets/codeSnippets/laravelBlog/Model.png')"/>
+            <p class="imgCap">Eloquent model for easy data access</p>
+        </div>
         <!-- Eloquent model -->
-        <!-- Contoller -->
+        <!-- Controller -->
+        <p>
+            The controller is the brains of the operation. Here, we are using a Restful structure so we have the seven functions: show, index, edit, update, create, store and delete. These will either return
+            an appropriate view or do work on the database. When one of the operations are done on the database, we have some validation to ensure the data received from the request has the neccessary 
+            data as well as nothing malicious.
+        </p>
+        <div class="imgDisplay">
+            <img :src="require('../assets/codeSnippets/laravelBlog/Controller.png')"/>
+            <p class="imgCap">Example function from Articles Controller</p>
+        </div>
+        <p>
+            One fo the big advantages of using Laravel is that we get User authentication out of the box. It acts as a middleware, a layer that all requests must pass through before reaching our controllers. 
+            Therefore there is a little need for us to handle the authentication ourselves and we can also access the information gathered by this layer using the Auth facade.
+        </p>
         <!-- Routes -->
+        <p>
+            For our routes, we need to map the requests to our controller functions. The HTTP methods we used are GET, POST, PUT and DELETE. GET requests return a view with POST, PUT and DELETE acting on the database.
+            Only POST is supported in html forms but this is another place where Laravel makes it easy for us, we can use @method directive which Laravel supplies and tells the server what actual method we are using.
+            Neat. The code below shows the mapping used for the routes to controller.
+        </p>
+        <div class="imgDisplay">
+            <img :src="require('../assets/codeSnippets/laravelBlog/Routes.png')"/>
+            <p class="imgCap">Routes defined in the web.php file</p>
+        </div>
         <!-- Views -->
+        <p>
+            For the views, we have a layout file which is used in every view to maintain a consistent site structure. All views extend this layout file and use the @section directives to define the content they display.
+            The code below shows out edit view for articles. This is a good example of the method directive which here we use PUT as we are looking to edit an existing record. We also require the @csrf directive which
+            laravel replaces with a hidden input that holds a token which then is used to verify that the form was sent by the web server and stop an cross site attack.
+        </p>
+        <div class="imgDisplay">
+            <img :src="require('../assets/codeSnippets/laravelBlog/View.png')"/>
+            <p class="imgCap">Edit article form view</p>
+        </div>
+        <p>
+            That concludes this post, there was a fair amount more work and code that went into the making of this website but for breivty sake I;ve just stuck to the implementation fo the articles. The full code can be inspected
+            in the github repo if you are interested in seeing more.
+        </p>
     </div>
 </template>
 
